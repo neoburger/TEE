c645e93c54caf8d9c801495043c16da94d3a8279
Your Name <you@example.com>
2021-11-09 09:57:14 +0000

    dev

---
+++ KeyGenerator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+using Neo.Wallets;
+
+namespace KeyGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sk = new byte[32];
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(sk);
+            }
+            var wif = new KeyPair(sk).Export();
+            Console.WriteLine(wif);
+        }
+    }
+}
