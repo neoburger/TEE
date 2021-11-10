baaef928e638c7670aba6f779bb1ab83c7a09a8b
vang1ong7ang <vang1ong7ang@outlook.com>
2021-11-10 01:45:53 -0500

    Init (#1)

---
+++ AddressPrinter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Neo.SmartContract;
+using Neo.Wallets;
+
+namespace AddressPrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"SCRIPTHASH = {Contract.CreateSignatureContract(new KeyPair(Wallet.GetPrivateKeyFromWIF(Environment.GetEnvironmentVariable("WIF"))).PublicKey).ScriptHash}");
+        }
+    }
+}
---
+++ KeyGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography;
+using Neo.Wallets;
+
+namespace KeyGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sk = new byte[32];
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create()) { rng.GetBytes(sk); }
+            Console.WriteLine(new KeyPair(sk).Export());
+        }
+    }
+}
