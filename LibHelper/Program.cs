4f09fc0904936d57b704fcca961444aa4a21ae21
Your Name <you@example.com>
2021-12-15 13:38:26 +0000

    dev

---
+++ LibHelper/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Neo;
+
+namespace LibHelper
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OK!");
+        }
+        public static bool HasBytes(this IEnumerable<byte[]> list, byte[] val) => list.Where(v => v.SequenceEqual(val)).Any();
+        public static bool HasBytes<T>(this IEnumerable<(byte[], T)> list, byte[] val) => list.Select(v => v.Item1).HasBytes(val);
+        public static T FindBytes<T>(this IEnumerable<(byte[], T)> list, byte[] val) => list.Where(v => v.Item1.SequenceEqual(val)).Single().Item2;
+        public static void SortBy<T>(this List<T> list, Func<T, IComparable> f) => list.Sort((x, y) => f(x).CompareTo(f(y)));
+        public static void Log<T>(this T val) => Console.Error.WriteLine(val);
+        public static void Out<T>(this T val) => Console.WriteLine(val);
+        public static readonly UInt160 BNEO = UInt160.Parse("0x48c40d4666f93408be1bef038b6722404d9a4c2a");
+    }
+}
---
+++ LibRPC/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using LibHelper;
+using Neo;
+using Neo.Network.P2P.Payloads;
+using Neo.Network.RPC;
+using Neo.Network.RPC.Models;
+using Neo.VM;
+using Neo.VM.Types;
+
+namespace LibRPC
+{
+    public static class Program
+    {
+        private static readonly string RPC = Environment.GetEnvironmentVariable("RPC");
+        private static readonly Uri URI = new(RPC);
+        private static readonly ProtocolSettings settings = ProtocolSettings.Load("/dev/stdin");
+        private static readonly RpcClient CLI = new(URI, null, null, settings);
+        static void Main(string[] args)
+        {
+            Call(new byte[] { ((byte)OpCode.RET) });
+            "OK!".Log();
+        }
+        public static StackItem[] Call(byte[] script)
+        {
+            RpcInvokeResult result = CLI.InvokeScriptAsync(script).GetAwaiter().GetResult();
+            if (result.State != VMState.HALT)
+            {
+                throw new Exception();
+            }
+            return result.Stack;
+        }
+        public static TransactionManagerFactory factory = new(CLI);
+        public static UInt256 Send(Transaction tx) => CLI.SendRawTransactionAsync(tx).GetAwaiter().GetResult();
+    }
+}
