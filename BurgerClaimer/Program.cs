afd825521668ecbb741fd748f336099b1665cd1a
Your Name <you@example.com>
2021-12-21 05:09:10 +0000

    dev

---
+++ BurgerClaimer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BurgerClaimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
