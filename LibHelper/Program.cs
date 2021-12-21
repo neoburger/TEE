b09310735ec4aa3f7d47049463101432192080d0
Your Name <you@example.com>
2021-12-21 12:10:04 +0000

    dev

--- LibHelper/Program.cs
+++ LibHelper/Program.cs
@@ -18,6 +18,7 @@ namespace LibHelper
         public static void Out<T>(this T val) => Console.WriteLine(val);
         public static UInt160 ToU160(this StackItem val) => new UInt160(val.GetSpan());
         public static byte[] ToBytes(this StackItem val) => val.GetSpan().ToArray();
+        public static IEnumerable<T> Nullize<T>(this IEnumerable<T> val) => val.Any() ? val : null;
         public static Neo.VM.Types.Array ToVMArray(this StackItem val) => (Neo.VM.Types.Array)val;
         public static Neo.VM.Types.Struct ToVMStruct(this StackItem val) => (Neo.VM.Types.Struct)val;
         public static void Assert(this bool val) { if (val == false) throw new Exception(); }
