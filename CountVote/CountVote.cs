74309e33849ecdde320fd2ff7df471a91773a0c7
Hecate2 <2474101468@qq.com>
2022-02-21 14:26:14 +0800

    Change to reading local files

--- CountVote/CountVote.cs
+++ CountVote/CountVote.cs
@@ -8,17 +8,15 @@ using Neo.SmartContract;
 using Neo.VM;
 using System.Numerics;
 using System.Text;
+using System.IO;
 using Newtonsoft.Json;
-using Octokit;
 using System.Collections.Generic;
 
 public class CountVote : Plugin, IPersistencePlugin
 {
-    private static readonly Credentials GITHUB_TOKEN = new(Environment.GetEnvironmentVariable("GITHUB_TOKEN") ?? throw new Exception("No GITHUB_TOKEN in environment variable"));
-    private GitHubClient client = new(new ProductHeaderValue("commitsClient"));
-    private static readonly string GITHUB_OWNER = Environment.GetEnvironmentVariable("GITHUB_OWNER") ?? "vang1ong7ang";
-    private static readonly string GITHUB_REPOSITORY = Environment.GetEnvironmentVariable("GITHUB_REPOSITORY") ?? "nbip";
-    private static readonly string GITHUB_BRANCH = Environment.GetEnvironmentVariable("GITHUB_BRANCH") ?? throw new Exception("No GITHUB_BRANCH in environment variable");
+    private static readonly string REPOSITORY_LOCAL_PATH = Environment.GetEnvironmentVariable("REPOSITORY_LOCAL_PATH") ?? throw new Exception("No REPOSITORY_LOCAL_PATH in environment variable");
+    // long NBIP_ID instead of ulong, because C# parses integers in json as long
+    private static readonly long NBIP_ID = long.TryParse(Environment.GetEnvironmentVariable("NBIP_ID"), out NBIP_ID) ? NBIP_ID : throw new Exception("No NBIP_ID in environment variable");
     private static readonly ulong COUNT_VOTE_SINCE_BLOCK = ulong.TryParse(Environment.GetEnvironmentVariable("COUNT_VOTE_SINCE_BLOCK"), out COUNT_VOTE_UNTIL_TIME) ? COUNT_VOTE_UNTIL_TIME : 0;
     private static readonly ulong COUNT_VOTE_UNTIL_TIME = ulong.TryParse(Environment.GetEnvironmentVariable("COUNT_VOTE_UNTIL_TIME"), out COUNT_VOTE_UNTIL_TIME) ? COUNT_VOTE_UNTIL_TIME : 99999999999990;
     private static readonly uint COUNT_VOTE_EVERY_BLOCKS = uint.TryParse(Environment.GetEnvironmentVariable("COUNT_VOTE_EVERY_BLOCKS"), out COUNT_VOTE_EVERY_BLOCKS) ? COUNT_VOTE_EVERY_BLOCKS : 21;
@@ -36,24 +34,25 @@ public class CountVote : Plugin, IPersistencePlugin
         {
             throw new Exception($"current time {currentTime} > COUNT_VOTE_UNTIL_TIME {COUNT_VOTE_UNTIL_TIME}");
         }
-        client.Credentials = GITHUB_TOKEN;
-        BigInteger votes = AnalyzeVoteFilesOfBranch(system, snapshot);
-        BigInteger totalSupply = ApplicationEngine.Run(DAO.MakeScript("totalSupply", new object[] {}), snapshot, settings: system.Settings).ResultStack.Select(v => v.GetInteger()).First();
-        Console.WriteLine($"Block{block.Index}:{GITHUB_BRANCH}:{votes}/{totalSupply}");
+        BigInteger votes = AnalyzeVoteFilesOfBranch(system, snapshot, path: REPOSITORY_LOCAL_PATH);
+        BigInteger totalSupply = ApplicationEngine.Run(DAO.MakeScript("totalSupply", new object[] { }), snapshot, settings: system.Settings).ResultStack.Select(v => v.GetInteger()).First();
+        Console.WriteLine($"Block{block.Index}:NBIP-{NBIP_ID}:{votes}/{totalSupply}");
         //throw new Exception("abort");
     }
 
-    BigInteger AnalyzeVoteFilesOfBranch(NeoSystem system, DataCache snapshot, string branch="")
+    BigInteger AnalyzeVoteFilesOfBranch(NeoSystem system, DataCache snapshot, string path = "")
     {
-        if(branch == "") { branch = GITHUB_BRANCH; }
+        
+        if (path == "") { path = REPOSITORY_LOCAL_PATH; }
         UInt160 voter;
         bool forOrAgainst;
         BigInteger totalVote = 0;
-        foreach (RepositoryContent file in GetBranchTree(branch))
+        foreach (string fileFullPath in Directory.GetFiles(path, "0x*.json"))
         {
-            if(file.Name.StartsWith("0x") && file.Name.EndsWith(".json") && file.Name.Length == 47)
+            string filename = System.IO.Path.GetFileName(fileFullPath);
+            if (filename.StartsWith("0x") && filename.EndsWith(".json") && filename.Length == 47)
             {
-                Tuple<UInt160, bool> result = VerifyVoteFile(file.Path, GITHUB_BRANCH);
+                Tuple<UInt160, bool> result = VerifyVoteFile(filename, path: System.IO.Path.GetDirectoryName(fileFullPath)??"");
                 voter = result.Item1; forOrAgainst = result.Item2;
                 if (voter == UInt160.Zero) { continue; }
                 ApplicationEngine balanceOfVoterRun = ApplicationEngine.Run(DAO.MakeScript("balanceOf", new object[] { voter }), snapshot, settings: system.Settings);
@@ -65,19 +64,15 @@ public class CountVote : Plugin, IPersistencePlugin
         return totalVote;
     }
 
-    Tuple<UInt160, bool> VerifyVoteFile(string path, string branch="")
+    Tuple<UInt160, bool> VerifyVoteFile(string filename, string path = "")
     {
-        if (branch == "") { branch = GITHUB_BRANCH; }
-        long nbipID = long.Parse(branch[5..]);
-        var contentClient = client.Repository.Content;
-        var task = contentClient.GetRawContentByRef(GITHUB_OWNER, GITHUB_REPOSITORY, path, GITHUB_BRANCH);
-        task.Wait();
-        byte[] content = task.Result;
+        if (path == "") { path = REPOSITORY_LOCAL_PATH; }
+        string text = System.IO.File.ReadAllText(System.IO.Path.Combine(path, filename));
         try
         {
-            Dictionary<string, object> json = JsonConvert.DeserializeObject<Dictionary<string, object>>(Encoding.UTF8.GetString(content));
-            UInt160 voter = UInt160.Parse(path[..42]);
-            if ((long)json["NBIP"] != nbipID) throw new Exception("Wrong NBIP ID in json");
+            Dictionary<string, object> json = JsonConvert.DeserializeObject<Dictionary<string, object>>(text);
+            UInt160 voter = UInt160.Parse(filename[..42]);
+            if ((long)json["NBIP"] != NBIP_ID) throw new Exception("Wrong NBIP ID in json");
             switch (json["PROFILE"])
             {
                 case "NEOLINE":
@@ -88,45 +83,24 @@ public class CountVote : Plugin, IPersistencePlugin
                     if (Contract.CreateSignatureRedeemScript(publicKey).ToScriptHash() != voter) { throw new Exception("Public key does not match scripthash"); }
                     bool forOrAgainst = (bool)json["YES"];
                     string forOrAganistString = forOrAgainst ? "FOR" : "AGAINST";
-                    string message = $"I VOTE {forOrAganistString} {branch}.";
-                    if(message.Length > 64) { throw new Exception("Invalid message"); }
+                    string message = $"I VOTE {forOrAganistString} NBIP-{NBIP_ID}.";
+                    if (message.Length > 64) { throw new Exception("Invalid message"); }
                     List<byte> signedMessage = (new byte[] { 0x01, 0x00, 0x01, 0xf0, (byte)(message.Length+32) }).ToList();
                     signedMessage = signedMessage.Concat(Encoding.ASCII.GetBytes(extra[..32])).ToList();
                     signedMessage = signedMessage.Concat(Encoding.ASCII.GetBytes(message).ToList()).ToList();
                     signedMessage = signedMessage.Concat(new byte[] { 0x00, 0x00 }).ToList();
-                    if(!Crypto.VerifySignature(signedMessage.ToArray(), Convert.FromHexString(signature), publicKey)) { throw new Exception("Invalid signature"); }
+                    if (!Crypto.VerifySignature(signedMessage.ToArray(), Convert.FromHexString(signature), publicKey)) { throw new Exception("Invalid signature"); }
                     return new Tuple<UInt160, bool>(voter, forOrAgainst);
                 default:
                     throw new Exception("Invalid profile. Only NEOLINE is supported for now.");
             }
-        }catch (Exception ex)
+        }
+        catch (Exception ex)
         {
-            Console.WriteLine(task.Result);
+            Console.WriteLine(path, filename);
+            Console.WriteLine(text);
             Console.WriteLine(ex);
             return new Tuple<UInt160, bool>(UInt160.Zero, false);
         }
-    }
-
-    IReadOnlyList<RepositoryContent> GetBranchTree(string branch="")
-    {
-        if(branch == "") { branch = GITHUB_BRANCH; }
-        var contentClient = client.Repository.Content;
-        var task = contentClient.GetAllContentsByRef(owner: GITHUB_OWNER, name: GITHUB_REPOSITORY, reference: branch);
-        task.Wait();
-        return task.Result;
-    }
-
-    static IReadOnlyList<GitHubCommit> GetBranchCommits()
-    {
-        var client = new GitHubClient(new ProductHeaderValue("commitsClient"))
-        {
-            Credentials = GITHUB_TOKEN
-        };
-        var commitClient = client.Repository.Commit;
-        var commitRequest = new CommitRequest { Sha=GITHUB_BRANCH };
-        var apiOptions = new ApiOptions { PageCount=int.MaxValue, StartPage=1, PageSize=100 };
-        var task = commitClient.GetAll(owner: GITHUB_OWNER, name: GITHUB_REPOSITORY, request: commitRequest, options: apiOptions);
-        task.Wait();
-        return task.Result;
     }
 }
