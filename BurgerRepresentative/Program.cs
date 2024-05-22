61ea3a37adaf5dbe57d4033f51c87150a20051cd
vang1ong7ang <vang1ong7ang@outlook.com>
2024-05-22 22:47:58 +0800

    Update Program.cs (#56)

--- BurgerRepresentative/Program.cs
+++ BurgerRepresentative/Program.cs
@@ -14,12 +14,12 @@ namespace BurgerRepresentative
     class Program
     {
         private static readonly BigInteger THREASHOLD = BigInteger.Parse(Environment.GetEnvironmentVariable("THREASHOLD"));
+        private static readonly BigInteger TARGET = UInt160.Parse(Environment.GetEnvironmentVariable("TARGET") ?? "0x48c40d4666f93408be1bef038b6722404d9a4c2a");
+
 
         static void Main(string[] args)
         {
-            UInt160 BNEO = UInt160.Parse("0x48c40d4666f93408be1bef038b6722404d9a4c2a");
             UInt160 REPRESENTATIVE = UInt160.Parse("0x329aeff39c13550337f02296d5ffc82583acaba3");
-
             BigInteger BLOCKNUM = NativeContract.Ledger.Hash.MakeScript("currentIndex").Call().Single().GetInteger();
             BigInteger GASBALANCE = NativeContract.GAS.Hash.MakeScript("balanceOf", new object[]{ REPRESENTATIVE }).Call().Single().GetInteger();
 
@@ -30,7 +30,7 @@ namespace BurgerRepresentative
                 return;
             }
 
-            byte[] REWARD = NativeContract.GAS.Hash.MakeScript("transfer", new object[]{ REPRESENTATIVE, BNEO, GASBALANCE-1_0000_0000, "reward"});
+            byte[] REWARD = NativeContract.GAS.Hash.MakeScript("transfer", new object[]{ REPRESENTATIVE, TARGET, GASBALANCE-1_0000_0000, "reward"});
 
             REWARD.SendTx().Out();
         }
